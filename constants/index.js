42bec115bd233adb0fc61c044bf6d15ccf86492e
keyvan <108103796+altcoinerkeyvan@users.noreply.github.com>
2023-07-24 18:01:34 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1fbf7746165f1a85d3e8af728c994267572f22df";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
